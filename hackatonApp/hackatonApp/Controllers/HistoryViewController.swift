13138496647a0b362bffc48f5d1cb075ad7aa1ce
jullynobre <jullynobre1@gmail.com>
2018-11-19 07:04:49 -0300

    Updates in presentation

--- hackatonApp/hackatonApp/Controllers/HistoryViewController.swift
+++ hackatonApp/hackatonApp/Controllers/HistoryViewController.swift
@@ -12,23 +12,40 @@ class HistoryViewController: UIViewController {
 
     @IBOutlet weak var pointsView: UIView!
     
+    @IBOutlet weak var pointsTableView: UITableView!
+    
+    let cellItendifier = "occurrencyCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.loadPointsView()
+        
+        self.pointsTableView.register(UINib.init(nibName: "OccurrencyTableViewCell", bundle: nil), forCellReuseIdentifier: self.cellItendifier)
+        self.pointsTableView.rowHeight = 100
+        self.pointsTableView.delegate = self
+        self.pointsTableView.dataSource = self
+    }
+    func loadPointsView() {
         let view = UINib(nibName: "PointsView", bundle: .main).instantiate(withOwner: nil, options: nil).first as! PointsView
         view.frame = self.pointsView.bounds
         self.pointsView.addSubview(view)
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellItendifier, for: indexPath) as! OccurrencyTableViewCell
+        return cell
+    }
+    
+    
 }
